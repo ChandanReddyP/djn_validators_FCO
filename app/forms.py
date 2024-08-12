7006c1269a5b50fb46bcaa855bf5e2a56e03048a
ChandanReddyP <chandanreddyp777@gmail.com>
2024-08-12 22:52:58 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,16 @@ class StudentForm(forms.Form):
     sa=forms.IntegerField()
     em=forms.EmailField()
     rem=forms.EmailField()
-
+    botcatcher=forms.CharField(widget=forms.HiddenInput,required=False)
 
     def clean(self):
         em=self.cleaned_data['em']
         rem=self.cleaned_data['rem']
         if em != rem:
             raise forms.ValidationError('emails is not matching')
+
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>=1:
+            raise forms.ValidationError('bot')
